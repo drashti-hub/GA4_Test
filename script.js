b2b6fe198afd6f1c5aefbf7f5fd5dad45c9e47df
drashti-hub <75781911+drashti-hub@users.noreply.github.com>
2023-11-02 15:31:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-// Get all the section links
-const sectionLinks = document.querySelectorAll('nav ul li a');
-var sectionName;
-document.querySelectorAll('nav ul li ').forEach(function(liElement) {
-         sectionName = liElement.textContent;
-})
-// Function to create event schema
-function createEventSchema(sectionId) {
-    const eventSchema = {
-        'event': 'click',
-        '@type': 'Event',
-        'section_name': `Viewed ${sectionId}`,
-        'cta_text':sectionName,
-        'location': window.location.href,
-        'parameters': {
-           '@type': 'ViewAction',
-            'target': sectionId
-        }
-    };
-    return JSON.stringify(eventSchema); // Convert object to JSON string
-}
+const measurementId = `G-3YQV0CL029`;
+const apiSecret = `TJRuaCk_QCKfPAX3df8U1g`;
 
-// Add click event listener to each section link
-sectionLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        const sectionId = link.getAttribute('href'); // Get the section ID from href attribute
-        const eventSchema = createEventSchema(sectionId);
+  var sheet = SpreadsheetApp.openById(1kCMiZr1hI0wIJ5a4ndNRh96h3awata1L01v2UyRLX7I).getSheetByName(Sheet1);
 
-        // Send event schema to Google Apps Script to log in Google Sheet
-        console.log(eventSchema);
-    });
+//var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var city = sheet.getRange("C").getValues();
+  var villa = sheet.getRange("D").getValues();
+
+  // Fetch the client_id from a specific cell in the Google Sheet (e.g., A3)
+  var client_id = sheet.getRange("A").getValue();
+
+fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurementId}&api_secret=${apiSecret}`, {
+  method: "POST",
+  body: JSON.stringify({
+    "client_id": "XXXXXXXXXX.YYYYYYYYYY",
+    "events": [{
+      "name": "refund",
+      "params": {
+        "currency": "USD",
+        "value": "9.99",
+        "transaction_id": "ABC-123"
+      }
+    }]
+  })
 });
+
+  // Log the response (optional)
+  Logger.log(response.getContentText());
+}
